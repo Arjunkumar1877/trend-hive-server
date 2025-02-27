b46073f5a2e5dc249b3ed31cda5ddef6cadc3643
Arjun kumar vs <arjun.tech177@gmail.com>
2025-02-28 01:53:58 +0530

    feat: postgres db set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { config } from 'dotenv';
+import { join } from 'path';
+
+config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.NEST_DB_HOST,
+      port: Number(process.env.NEST_DB_PORT),
+      username: process.env.NEST_DB_USERNAME,
+      password: process.env.NEST_DB_PASSWORD,
+      database: 'ecommerce_db',
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
