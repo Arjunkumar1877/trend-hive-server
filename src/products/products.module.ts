569183902d065d92589c7959b74a9c6be78528d3
Arjunkumar1877 <arjun.tech177@gmail.com>
2025-04-04 00:18:51 +0530

    feat: product add , update and delete

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { Product } from '../data/entities/product.entity';
+import { Category } from '../data/entities/category.entity';
+import { Image } from '../data/entities/image.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product, Category, Image])],
+  controllers: [ProductsController],
   providers: [ProductsService],
-  controllers: [ProductsController]
+  exports: [ProductsService],
 })
 export class ProductsModule {}
