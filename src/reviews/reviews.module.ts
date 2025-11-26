dfe5c13f8b45a5297207d8a7d2ac6bf07eb8fe9f
Arjunkumar1877 <arjun.tech177@gmail.com>
2025-11-26 21:16:54 +0530

    feat: review module

---
+++ src/reviews/reviews.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ReviewsController } from './reviews.controller';
+import { ReviewsService } from './reviews.service';
+import { Review, ReviewSchema } from '../data/schemas/review.schema';
+import { Product, ProductSchema } from '../data/schemas/product.schema';
+import { User, UserSchema } from '../data/schemas/user.schema';
+import { Order, OrderSchema } from '../data/schemas/order.schema';
+import { FirebaseModule } from '../firebase/firebase.module';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Review.name, schema: ReviewSchema },
+      { name: Product.name, schema: ProductSchema },
+      { name: User.name, schema: UserSchema },
+      { name: Order.name, schema: OrderSchema },
+    ]),
+    FirebaseModule,
+    UsersModule,
+  ],
+  controllers: [ReviewsController],
+  providers: [ReviewsService],
+  exports: [ReviewsService],
+})
+export class ReviewsModule {}
+
